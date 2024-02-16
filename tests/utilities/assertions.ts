68c37a79afee58c24d41b1c42ae71d0c0f7d6b2c
Bakry13 <mohamed.bakry13@gmail.com>
2024-02-16 17:01:37 +0200

    add more assertions in assertions module

--- tests/utilities/assertions.ts
+++ tests/utilities/assertions.ts
@@ -1,5 +1,6 @@
 import { expect, type Locator } from '@playwright/test';
 
+//========================Hard assertion=======================
 async function assertElementText( locator:Locator, expectedText:any, language:string) {
     // const language = process.env.LANGUAGE!; //we can use this command directly if we do not use BDD wihout passing the language
     let textValue;
@@ -15,4 +16,51 @@ async function assertElementText( locator:Locator, expectedText:any, language:st
     await expect(locator).toHaveText(textValue);
 }
 
-export default {assertElementText}
+async function assertElementExist( locator:Locator) {
+    await expect(locator).toBeVisible();
+}
+
+async function assertElementNotExist( locator:Locator) {
+    await expect(locator).not.toBeVisible();
+}
+
+async function assertElementEnabled( locator:Locator) {
+    await expect(locator).toBeEnabled();
+}
+
+async function assertElementNotEnabled( locator:Locator) {
+    await expect(locator).not.toBeEnabled();
+}
+//===================Soft assertions=========================
+async function validateElementText( locator:Locator, expectedText:any, language:string) {
+    // const language = process.env.LANGUAGE!; //we can use this command directly if we do not use BDD wihout passing the language
+    let textValue;
+
+    if (language.toLowerCase().includes("en"))
+    {    
+        textValue = expectedText.en;
+    }
+    else if (language.toLowerCase().includes("ar"))
+    {
+        textValue = expectedText.ar;
+    }
+    await expect.soft(locator).toHaveText(textValue);
+}
+
+async function validateElementExist( locator:Locator) {
+    await expect.soft(locator).toBeVisible();
+}
+
+async function validateElementNotExist( locator:Locator) {
+    await expect.soft(locator).not.toBeVisible();
+}
+
+async function validateElementEnabled( locator:Locator) {
+    await expect.soft(locator).toBeEnabled();
+}
+
+async function validateElementNotEnabled( locator:Locator) {
+    await expect.soft(locator).not.toBeEnabled();
+}
+export default { assertElementText, assertElementExist, assertElementNotExist, assertElementEnabled, assertElementNotEnabled,
+    validateElementText, validateElementExist, validateElementNotExist, validateElementEnabled, validateElementNotEnabled }
